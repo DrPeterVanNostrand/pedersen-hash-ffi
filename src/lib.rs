ddfa033b6bda5b2793381a5a0f766a6a98c0b786
DrPeterVanNostrand <jnz@riseup.net>
2019-11-20 17:54:49 +0000

    Change pedersen hash to accept any length of preimage

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,16 @@ use fil_sapling_crypto::jubjub::JubjubBls12;
 use fil_sapling_crypto::pedersen_hash::{pedersen_hash as sapling_pedersen_hash, Personalization};
 use paired::bls12_381::Bls12;
 
-pub fn pedersen_hash(preimage: &[u8]) -> [u8; 32] {
-    let params = JubjubBls12::new();
+const N_BITS_PER_SEGMENT: usize = 189;
+const WINDOW_SIZE: u32 = 8;
 
+pub fn pedersen_hash(preimage: &[u8]) -> [u8; 32] {
     let preimage_bits = BitVec::<bitvec::LittleEndian, u8>::from(preimage);
     let preimage_bit_len = 8 * preimage.len();
+    let n_segments = (preimage_bit_len as f32 / N_BITS_PER_SEGMENT as f32).ceil() as usize;
+
+    let params =
+        JubjubBls12::new_with_n_segments_and_window_size(n_segments, WINDOW_SIZE, None).unwrap();
 
     let digest_fr = sapling_pedersen_hash::<Bls12, _>(
         Personalization::None,
